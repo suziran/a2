0c2696195e9d216d003f35fdbe9861f362a1ce20
suziran <suziranv@gmail.com>
2017-07-04 14:43:03 +0000

    replace form with simple form bootstrap template

--- a2/app/controllers/groups_controller.rb
+++ a2/app/controllers/groups_controller.rb
@@ -15,7 +15,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-         redirect_to groups_path
+      redirect_to groups_path
     else
       render :new
     end
